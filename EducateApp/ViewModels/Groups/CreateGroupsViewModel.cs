dd3086a48f24d7603430bea75a5fe3fb7698d16e
Asvir <asv098@mail.ru>
2021-12-09 22:56:46 +0500

    Добавление работы с таблицей "Группы" со связанными списками

--- EducateApp/ViewModels/Groups/CreateGroupsViewModel.cs
+++ EducateApp/ViewModels/Groups/CreateGroupsViewModel.cs
@@ -1,29 +1,28 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
-namespace EducateApp.Models.Data
+namespace EducateApp.ViewModels.Groups
 {
-    public class Group
+    public class CreateGroupViewModel
     {
-        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Display(Name = "ИД")]
-        public short Id { get; set; }
+        [Required]
+        [Display(Name = "Выберите форму обучения")]
+        public short IdFormOfStudy { get; set; }
 
         [Required]
-        [Display(Name = "Специальность")]
+        [Display(Name = "Выберите специальность")]
         public short IdSpecialty { get; set; }
 
         [Required(ErrorMessage = "Введите название группы")]
         [Display(Name = "Группа")]
         public string Name { get; set; }
 
-        [Display(Name = "Кол. студентов")]
+        [Display(Name = "Количество студентов")]
         [Range(1, 255, ErrorMessage = "Введите число от 1 до 255")]
         public byte? CountOfStudents { get; set; }
 
         [Required(ErrorMessage = "Введите год поступления")]
-        [Display(Name = "Год пост.")]
+        [Display(Name = "Год поступления")]
         [Range(1950, 9999, ErrorMessage = "Введите корректное число")]
         public short YearOfAdmission { get; set; }
 
@@ -33,17 +32,11 @@ namespace EducateApp.Models.Data
         public short YearOfIssue { get; set; }
 
         [Required(ErrorMessage = "Введите имя классного руководителя")]
-        [Display(Name = "Имя кл. рук.")]
+        [Display(Name = "Имя классного руководителя")]
         [DataType(DataType.Text)]
         public string ClassTeacher { get; set; }
 
-        [Display(Name = "Контакты кл. рук.")]
+        [Display(Name = "Контакты классного руководителя")]
         public string ContactsTeacher { get; set; }
-
-
-        // Навигационные свойства
-        [Display(Name = "Специальность")]
-        [ForeignKey("IdSpecialty")]
-        public Specialty Specialty { get; set; }
     }
 }
