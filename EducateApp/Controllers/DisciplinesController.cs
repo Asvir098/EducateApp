4c4d369c96166a6d941823f6cd30dd8193aae945
Asvir <asv098@mail.ru>
2021-12-24 10:00:28 +0500

    Скачивание шаблона Excel для таблицы "Дисциплины"

--- EducateApp/Controllers/DisciplinesController.cs
+++ EducateApp/Controllers/DisciplinesController.cs
@@ -1,10 +1,13 @@
-﻿using EducateApp.Models;
+﻿using ClosedXML.Excel;
+using EducateApp.Models;
 using EducateApp.Models.Data;
 using EducateApp.ViewModels.Disciplines;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -206,6 +209,55 @@ namespace EducateApp.Controllers
             }
 
             return PartialView(discipline);
+        }
+
+        public async Task<FileResult> DownloadPattern()
+        {
+            IdentityUser user = await _userManager.FindByNameAsync(HttpContext.User.Identity.Name);
+
+            int i = 1;      // счетчик
+
+            IXLRange rngBorder;     // объект для работы с диапазонами в Excel (выделение групп ячеек)
+
+            // создание книги Excel
+            using (XLWorkbook workbook = new(XLEventTracking.Disabled))
+            {
+                // добавить лист в книгу Excel
+                IXLWorksheet worksheet = workbook.Worksheets
+                    .Add($"Дисциплины");
+
+                // заголовки у столбцов
+                worksheet.Cell("A" + i).Value = "Индекс профессионального модуля";
+                worksheet.Cell("B" + i).Value = "Название профессионального модуля";
+                worksheet.Cell("C" + i).Value = "Индекс";
+                worksheet.Cell("D" + i).Value = "Название";
+                worksheet.Cell("E" + i).Value = "Краткое название";
+
+                // устанавливаем внешние границы для диапазона A4:F4
+                rngBorder = worksheet.Range("A1:E1");       // создание диапазона (выделения ячеек)
+                rngBorder.Style.Border.OutsideBorder = XLBorderStyleValues.Thin;       // для диапазона задаем внешнюю границу
+
+                // на листе для столбцов задаем значение ширины по содержимому
+                worksheet.Columns().AdjustToContents();
+
+                // счетчик "обнуляем"
+                i = 1;
+
+                // создаем стрим
+                using (MemoryStream stream = new())
+                {
+                    // помещаем в стрим созданную книгу
+                    workbook.SaveAs(stream);
+                    stream.Flush();
+
+                    // возвращаем файл определенного типа
+                    return new FileContentResult(stream.ToArray(),
+                        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+                    {
+                        FileDownloadName = $"disciplines_{DateTime.UtcNow.ToShortDateString()}.xlsx"     //в названии файла указываем таблицу и текущую дату
+                    };
+                }
+            }
         }
 
         private bool DisciplineExists(short id)
